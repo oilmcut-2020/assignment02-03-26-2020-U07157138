dc1369e2670502e73fd2cd7cfa459bd48fc8cd8d
U07157138 <49745864+U07157138@users.noreply.github.com>
2020-03-26 21:28:07 +0800

    Add files via upload

---
+++ Week 03/Prime.java
@@ -0,0 +1,21 @@
+public class Prime {
+    public static void main(String[] args) {
+        
+        
+        int i, j;
+        for (i = 2; i <= 100; i++) {
+             for (j = 2; j <= i; j++) {
+                if (i % j == 0)
+                    
+                    break;
+                    
+             }
+            if (j >= i)
+                 System.out.println(i);
+         }
+    }
+}
+
+             
+
+
